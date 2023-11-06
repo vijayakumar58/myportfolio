eb47dbd4fe147cfa32b1369391d3a656613dfbc3
vijayakumar58 <vijayalingam58@gmail.com>
2023-11-06 18:10:05 +0530

    add

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -7,9 +7,9 @@ const Skills = () => {
   return (
     <section
       id="skills"
-      className="w-full py-20 border-b-[1px] border-b-black"
+      className="w-full py-10 border-b-[1px] border-b-black"
     >
-      <Title title="Skills" des="What I Do" />
+      <Title title="Passionate Web Developer with a flair for creating stunning, user-friendly websites" des="Skills" />
       <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-20">
         {skillsData.map((item) => (
           <Card item={item} key={item.id} />
--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 import { AiFillHtml5 } from "react-icons/ai";
 import { DiCss3, DiMongodb, } from "react-icons/di";
-import { FaReact, FaNodeJs, FaAws} from "react-icons/fa";
-import { SiJavascript, SiExpress, SiMysql } from "react-icons/si";
+import { FaReact, FaNodeJs, FaAws, FaBootstrap} from "react-icons/fa";
+import { SiJavascript, SiMysql } from "react-icons/si";
 
 // Features Data
 export const skillsData = [
@@ -9,54 +9,54 @@ export const skillsData = [
     id: 1,
     icon: <AiFillHtml5 />,
     title: "HTML5",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "HTML5 the core language for structuring and presenting web content, allowing for dynamic and visually appealing web pages.",
   },
   {
     id: 2,
     icon: <DiCss3 />,
     title: "CSS3",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "CSS3 is another highlight, as it enables me to style and design web elements beautifully, enhancing the overall user experience on my projects.",
   },
   {
     id: 3,
-    icon: <SiJavascript />,
-    title: "JavaScript",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    icon: <FaBootstrap />,
+    title: "Bootstrap",
+    des: "Bootstrap: Experienced in leveraging Bootstrap to expedite frontend development, enhancing UI consistency and responsiveness.",
   },
   {
     id: 4,
-    icon: <FaReact />,
-    title: "ReactJS",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    icon: <SiJavascript />,
+    title: "JavaScript",
+    des: "JavaScript is a key tool in my portfolio, as it empowers me to create interactive and responsive web applications, adding functionality and interactivity to websites.",
   },
   {
     id: 5,
-    icon: <FaNodeJs />,
-    title: "NodeJS",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    icon: <FaReact />,
+    title: "ReactJS",
+    des: "React.js is a major strength, allowing me to build efficient and maintainable user interfaces with its component-based architecture.",
   },
   {
     id: 6,
-    icon: <SiExpress />,
-    title: "ExpressJS",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    icon: <FaNodeJs />,
+    title: "NodeJS",
+    des: "Node.js and Express.js enable me to develop server-side applications and APIs, facilitating seamless communication between the frontend and backend.",
   },
   {
     id: 7,
     icon: <DiMongodb />,
     title: "MongoDB",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "MongoDB is a NoSQL database that I leverage to work with unstructured or semi-structured data, providing flexibility in handling diverse data formats.",
   },
   {
     id: 8,
     icon: <SiMysql/>,
     title: "MySQL",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "MySQL is a database management system I utilize to store and manage structured data efficiently in my portfolio projects.",
   },
   {
     id: 9,
     icon: <FaAws />,
     title: "AWS",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque solutahic consequuntur eum repellendus ad",
+    des: "My portfolio demonstrates AWS expertise, showcasing my ability to deploy, scale, and manage web applications and services in the cloud for optimal performance and reliability.",
   },
 ];
